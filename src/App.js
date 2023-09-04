4b1e2eedfb8ea20f0a804616bf8340621d454465
Jake McGuire <dev.jmcguire@gmail.com>
2023-09-03 20:26:24 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react" 
 
 function App() {
+
+  let [color, setColor] = useState("#000000");
+  let [addedColors, setAddedColors] = useState([]);
+
+  function addColor(color) {
+    let newColors = [...addedColors];
+    newColors.push(color);
+    setAddedColors(newColors);
+    console.log(addedColors);
+  }
+
+  function removeColor(color) {
+    let newColors = [...addedColors];
+    let index = newColors.indexOf(color);
+    if (index > -1) {
+      newColors.splice(index, 1);
+    }
+    setAddedColors(newColors);
+    console.log(addedColors);
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="main"> 
+          <div className="tile main-tile" style={{backgroundColor: color}}>
+            <input type="color" value={color} onChange={e => setColor(e.target.value)} />
+            <button onClick={() => addColor(color)}>Add</button>
+          </div>
+          {addedColors.map(addedColor => {
+            return (
+              <div className="tile" style={{backgroundColor: addedColor}}>
+                <button onClick={() => removeColor(addedColor)}>Remove</button>
+            </div>
+            );
+          })}
+        </div> 
     </div>
   );
 }
